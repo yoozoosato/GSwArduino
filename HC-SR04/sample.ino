c43bbd91bad3654d7b02561eb0f53d165b215cea
SATO Yozo <yoozoosato@gmail.com>
2013-06-22 18:54:53 +0900

    距離に応じて光らせる LED の色を分けるという小細工を実装

--- HC-SR04/sample.ino
+++ HC-SR04/sample.ino
@@ -2,13 +2,15 @@
 
 #define trigPin 8
 #define echoPin 9
-#define LED 13
+#define LED_YELLOW 13
+#define LED_RED 12
 
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(LED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
 }
 
 void loop() {
@@ -25,14 +27,24 @@ void loop() {
   // 往復してるので半分に割って、音の速度で割ると距離が出る
   distance = (duration/2) / 29.1;
 
-  if (distance >= 300 || distance <= 0){
-    Serial.println("Out of range");
-	digitalWrite(LED, HIGH);
+  if (distance <= 10){
+    Serial.println("Out of range: Too Close");
+	digitalWrite(LED_RED, HIGH);
+	digitalWrite(LED_YELLOW, LOW);
   }
+  else if (distance <= 25) {
+	digitalWrite(LED_YELLOW, HIGH);
+	digitalWrite(LED_RED, LOW);
+  }	
+  /* else if (distance >= 40) { */
+  /*   Serial.println("Out of range: Too Far"); */
+  /* 	digitalWrite(LED, HIGH); */
+  /* } */
   else {
     Serial.print(distance);
     Serial.println(" cm");
-	digitalWrite(LED, LOW);
+	digitalWrite(LED_YELLOW, LOW);
+	digitalWrite(LED_RED, LOW);
   }
   delay(500);
 }
