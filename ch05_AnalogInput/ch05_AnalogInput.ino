3dceefeb0369a11a7cfff30d222d806f8c2ebae4
SATO Yozo <yoozoosato@gmail.com>
2013-06-22 13:50:52 +0900

    p.55 Cds セルから読み取った値で LED の明るさを変える
    センサーの反応が微妙な気がするけれど、実現したいことはできている感じ。
    入力された値をデバッグ表示するのとか、どうするんだろう？
    js とかの console.log 的なことがやりたい。

---
+++ ch05_AnalogInput/ch05_AnalogInput.ino
@@ -0,0 +1,16 @@
+const int LED = 13;
+int val = 0;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  val = analogRead(0);
+  
+  digitalWrite(13, HIGH);
+  delay(val);
+  
+  digitalWrite(13, LOW);
+  delay(val);
+}
