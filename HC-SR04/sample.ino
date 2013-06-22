688f14ce24ea8a0554ad3a23a8f9e585e5da6e7d
SATO Yozo <yoozoosato@gmail.com>
2013-06-22 18:22:32 +0900

    コメントを追記した。

--- HC-SR04/sample.ino
+++ HC-SR04/sample.ino
@@ -13,11 +13,18 @@ void setup() {
 
 void loop() {
   int duration, distance;
+
+  // 超音波を投げる
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(1000);
   digitalWrite(trigPin, LOW);
+
+  // 反射して返ってきたのを受け取る
   duration = pulseIn(echoPin, HIGH);
+
+  // 往復してるので半分に割って、音の速度で割ると距離が出る
   distance = (duration/2) / 29.1;
+
   if (distance >= 300 || distance <= 0){
     Serial.println("Out of range");
   }
