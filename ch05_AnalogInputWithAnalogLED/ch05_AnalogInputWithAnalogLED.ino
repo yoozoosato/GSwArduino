eb000363465de9f4c73f9bbbb3ea350b14f5ff21
SATO Yozo <yoozoosato@gmail.com>
2013-06-22 14:00:53 +0900

    一瞬眩く点灯する時があるけど、まぁ、許容範囲かな。。

--- ch05_AnalogInputWithAnalogLED/ch05_AnalogInputWithAnalogLED.ino
+++ ch05_AnalogInputWithAnalogLED/ch05_AnalogInputWithAnalogLED.ino
@@ -1,5 +1,6 @@
 const int LED = 9;
 int val = 0;
+int brightness = 0;
 
 void setup() {
   pinMode(LED, OUTPUT);
@@ -7,6 +8,7 @@ void setup() {
 
 void loop() {
   val = analogRead(0);
-  analogWrite(LED, val/2.5);
+  brightness = (val < 10 ? 1 : val / 2.5);
+  analogWrite(LED, brightness);
   delay(10);
 }
