1362a4a775ebc964e1b2bf5334d356c122d6e9b3
SATO Yozo <yoozoosato@gmail.com>
2013-06-22 17:44:55 +0900

    貰い物の距離センサーとサンプル
    シリアルの出力結果をパソコンで見るには、やっぱり Arduino IDE を起動しないといけないのか・・

---
+++ HC-SR04/sample.ino
@@ -0,0 +1,27 @@
+/* HC-SR04 Sample */
+
+#define trigPin 8
+#define echoPin 9
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  int duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance >= 300 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
